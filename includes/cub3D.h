b0d43d0dde8a32d490fbadf9c1b8b510cc7a92d6
Caio Cesar De Souza Scapucin <cacesar-@c1r4p3.42sp.org.br>
2026-03-30 18:14:41 -0300

    3d feito

--- includes/cub3D.h
+++ includes/cub3D.h
@@ -6,7 +6,7 @@
 /*   By: cacesar- <cacesar-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/16 17:17:33 by clados-s          #+#    #+#             */
-/*   Updated: 2026/03/24 08:51:16 by cacesar-         ###   ########.fr       */
+/*   Updated: 2026/03/30 16:30:26 by cacesar-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,31 @@
 # include "../libft/includes/libft.h"
 # include "../minilibx-linux/mlx.h"
 # include "../minilibx-linux/mlx_int.h"
-# include <math.h>
 # include <X11/X.h>
+# include <math.h>
+
+# define PI 3.1415926535
+# define DR 0.0174533
 
 typedef struct s_libx
 {
+	float			px;
+	float			py;
+	float			pdx;
+	float			pdy;
+	float			oldpy;
+	float			oldpx;
+	float			pa;
+	float			d_h;
+	float			hx;
+	float			hy;
+	float			d_v;
+	float			vx;
+	float			vy;
+	float			d_t;
+	float			l_h;
+	float			atan;
 	int				ll;
-	int				px;
-	int				py;
 	int				bpp;
 	int				endian;
 	int				matrix_s;
--- srcs/tracing/cub3D.c
+++ srcs/tracing/cub3D.c
@@ -6,7 +6,7 @@
 /*   By: cacesar- <cacesar-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/16 09:55:44 by cacesar-          #+#    #+#             */
-/*   Updated: 2026/03/24 14:32:34 by cacesar-         ###   ########.fr       */
+/*   Updated: 2026/03/30 18:12:01 by cacesar-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,24 +19,229 @@
 
 #include <stdio.h>
 
+static void	paint_m(t_win*lmx, float iy, float ix, char*i);
+static void	m_paint(t_win*lmx);
+
+static void	draw_background(t_win *lmx)
+{
+	int				x;
+	int				y;
+	unsigned int	*dst;
+
+	y = -1;
+	while (++y < 1024)
+	{
+		x = -1;
+		while (++x < 1920)
+		{
+			dst = (unsigned int *)(lmx->addr
+					+ (y * lmx->ll + x * (lmx->bpp / 8)));
+			if (y < 1024 / 2)
+				*dst = 0x333333;
+			else
+				*dst = 0x777777;
+		}
+	}
+}
+
+static void	draw_line(t_win *lmx, int x, int line_h, int vert)
+{
+	int		y;
+	int		start;
+	char	*dst;
+
+	start = (1024 / 2) - (line_h / 2);
+	if (start < 0)
+		start = 0;
+	y = -1;
+	while (++y < line_h && (y + start) < 1024)
+	{
+		dst = lmx->addr + ((y + start) * lmx->ll + x * (lmx->bpp / 8));
+		if (vert)
+			*(unsigned *)dst = 0x880000;
+		else
+			*(unsigned *)dst = 0xFF0000;
+	}
+}
+
+static float	dist(float ax, float ay, float bx, float by)
+{
+	return (sqrt((bx - ax) * (bx - ax) + (by - ay) * (by - ay)));
+}
+
+static void	rays(t_win*lmx, int r, float ra, float ratan)
+{
+	int		c;
+	float	rx;
+	float	ry;
+	float	xo;
+	float	yo;
+
+	if (ra < 0)
+		ra += 2 * PI;
+	if (ra > 2 * PI)
+		ra -= 2 * PI;
+	c = -1;
+	while (++c < r)
+	{
+		ratan = -1 / tan(ra);
+		lmx->atan = ratan;
+		if (lmx->atan > 100)
+			lmx->atan = 100;
+		if (lmx->atan < -100)
+			lmx->atan = -100;
+		lmx->d_h = 1000000;
+		lmx->hx = lmx->px;
+		lmx->hy = lmx->py;
+		if (ra > PI)
+		{
+			ry = (((int)lmx->py >> 6) << 6) - 0.0001;
+			rx = (lmx->py - ry) * ratan + lmx->px;
+			yo = -64;
+			xo = -yo * ratan;
+		}
+		if (ra < PI)
+		{
+			ry = (((int)lmx->py >> 6) << 6) + 64 + 0.0001;
+			rx = (lmx->py - ry) * ratan + lmx->px;
+			yo = 64;
+			xo = -yo * ratan;
+		}
+		if (ra < 0.001 || fabs(ra - PI) < 0.001)
+		{
+			rx = lmx->px;
+			ry = lmx->py;
+		}
+		while (1)
+		{
+			if ((int)ry >> 6 >= lmx->matrix_s || (int)rx >> 6 >= lmx->matrix_s
+				|| (int)rx >> 6 < 0 || (int)ry >> 6 < 0)
+				break ;
+			if (lmx->matrix[(int)ry >> 6][(int)rx >> 6] == 1)
+			{	
+				lmx->d_h = dist(lmx->px, lmx->py, rx, ry);
+				lmx->hx = rx;
+				lmx->hy = ry;
+				break ;
+			}
+			else
+			{
+				rx += xo;
+				ry += yo;
+			}
+		}
+		ratan = tan(ra);
+		lmx->atan = ratan;
+		if (lmx->atan > 100)
+			lmx->atan = 100;
+		if (lmx->atan < -100)
+			lmx->atan = -100;
+		lmx->d_v = 1000000;
+		lmx->vx = lmx->px;
+		lmx->vy = lmx->py;
+		if (ra > PI / 2 && ra < 3 * PI / 2)
+		{
+			rx = (((int)lmx->px >> 6) << 6) - 0.0001;
+			ry = (rx - lmx->px) * ratan + lmx->py;
+			xo = -64;
+			yo = xo * ratan;
+		}
+		if (ra < PI / 2 || ra > 3 * PI / 2)
+		{
+			rx = (((int)lmx->px >> 6) << 6) + 64 + 0.0001;
+			ry = (rx - lmx->px) * ratan + lmx->py;
+			xo = 64;
+			yo = xo * ratan;
+		}
+		if (ra < 0.001 || fabs(ra - PI) < 0.001)
+		{
+			rx = lmx->px;
+			ry = lmx->py;
+		}
+		while (1)
+		{
+			if ((int)ry >> 6 >= lmx->matrix_s || (int)rx >> 6 >= lmx->matrix_s
+				|| (int)rx >> 6 < 0 || (int)ry >> 6 < 0)
+				break ;
+			if (lmx->matrix[(int)ry >> 6][(int)rx >> 6] == 1)
+			{
+				lmx->d_v = dist(lmx->px, lmx->py, rx, ry);
+				lmx->vx = rx;
+				lmx->vy = ry;
+				break ;
+			}
+			else
+			{
+				rx += xo;
+				ry += yo;
+			}
+		}
+		if (lmx->d_v < lmx->d_h)
+		{
+			lmx->d_t = dist(lmx->px, lmx->py, lmx->vx, lmx->vy) * cos(ra - lmx->pa);
+			if (lmx->d_t < 0.1)
+				lmx->d_t = 0.1;
+			lmx->l_h = (64 * 1024) / lmx->d_t;
+			if (lmx->l_h > 1024)
+				lmx->l_h = 1024;
+			draw_line(lmx, c, lmx->l_h, 1);
+		}
+		else
+		{
+			lmx->d_t = dist(lmx->px, lmx->py, lmx->hx, lmx->hy) * cos(ra - lmx->pa);
+			if (lmx->d_t < 0.1)
+				lmx->d_t = 0.1;
+			lmx->l_h = (64 * 1024) / lmx->d_t;
+			if (lmx->l_h > 1024)
+				lmx->l_h = 1024;
+			draw_line(lmx, c, lmx->l_h, 0);
+		}
+		ra += (60 * DR) / 1920;
+		if (ra < 0)
+			ra += 2 * PI;
+		if (ra > 2 * PI)
+			ra -= 2 * PI;
+	}
+}
+
+static void	movement(t_win*lmx)
+{
+	if (lmx->a)
+		lmx->pa -= 0.1;
+	if (lmx->d)
+		lmx->pa += 0.1;
+	if (lmx->pa < 0)
+		lmx->pa += (2 * PI);
+	if (lmx->pa >= (2 * PI))
+		lmx->pa -= (2 * PI);
+	if (lmx->a || lmx->d)
+		lmx->pdx = cos(lmx->pa) * 5;
+	if (lmx->a || lmx->d)
+		lmx->pdy = sin(lmx->pa) * 5;
+	if (lmx->w)
+		lmx->px += lmx->pdx;
+	if (lmx->w)
+		lmx->py += lmx->pdy;
+	if (lmx->s)
+		lmx->px -= lmx->pdx;
+	if (lmx->s)
+		lmx->py -= lmx->pdy;
+}
+
 static void	limits(t_win*lmx)
 {
-	if (lmx->w)
-		lmx->py -= 2;
-	if (lmx->a)
-		lmx->px -= 2;
-	if (lmx->s)
-		lmx->py += 2;
-	if (lmx->d)
-		lmx->px += 2;
-	if (lmx->matrix[lmx->py / 64][(lmx->px - 2) / 64] == 1 && lmx->a)
-		lmx->px = (((lmx->px + 63) / 64) * 64) + 2;
-	if (lmx->matrix[(lmx->py - 2) / 64][lmx->px / 64] == 1 && lmx->w)
-		lmx->py = (((lmx->py + 63) / 64) * 64) + 2;
-	if (lmx->matrix[lmx->py / 64][(lmx->px + 2) / 64] == 1 && lmx->d)
-		lmx->px = (lmx->px - (lmx->px + 2) % 64) - 2;
-	if (lmx->matrix[(lmx->py + 2) / 64][lmx->px / 64] == 1 && lmx->s)
-		lmx->py = (lmx->py - (lmx->py + 2) % 64) - 2;
+	if (lmx->matrix[(int)lmx->py / 64][(int)lmx->px / 64] == 1)
+	{
+		if (lmx->matrix[(int)lmx->oldpy / 64][(int)lmx->px / 64] != 1)
+			lmx->py = lmx->oldpy;
+		else if (lmx->matrix[(int)lmx->py / 64][(int)lmx->oldpx / 64] != 1)
+			lmx->px = lmx->oldpx;
+		else
+		{
+			lmx->px = lmx->oldpx;
+			lmx->py = lmx->oldpy;
+		}
+	}
 }
 
 static int	close_window(t_win*lmx)
@@ -55,65 +260,61 @@ static int	close_window(t_win*lmx)
 	exit (0);
 }
 
-static void	paint_m(t_win*lmx, int iy, int ix)
+static void	blackout(t_win*lmx, int x, int y)
 {
-	int			x;
-	int			y;
-	char		*i;
+	char	*i;
 
-	x = 64 * ix - 1;
-	while (++x <= 64 * (ix + 1))
+	while (++x <= 320)
 	{
-		y = 64 * iy - 1;
-		while (++y <= 64 * (iy + 1))
+		y = -1;
+		while (++y <= 320)
 		{
 			i = lmx->addr + (y * lmx->ll + x * (lmx->bpp / 8));
-			if (lmx->matrix[iy][ix] == 1 && x >= 1 && y >= 1
-				&& x <= 64 *(ix + 1) - 2 && y <= 64 *(iy + 1) - 2
-				&& x <= lmx->px + 128 && x >= lmx->px - 128
-				&& y <= lmx->py + 128 && y >= lmx->py - 128)
-				*(unsigned *)i = 0xFF0000;
-			else
-				*(unsigned *)i = 0x000000;
-			if (x >= lmx->px - 2 && x <= lmx->px + 2
-				&& y >= lmx->py - 2 && y <= lmx->py + 2
-				&& lmx->matrix[lmx->py / 64][ix] != 1
-				&& lmx->matrix[iy][lmx->px / 64] != 1)
-				*(unsigned *)i = 0x008000;
+			*(unsigned *)i = 0x000000;
 		}
 	}
 }
 
-static int	m_paint(void*lm)
+static int	paint(void*lm)
+{
+	t_win		*lmx;
+	int			c;
+
+	lmx = (t_win *)lm;
+	c = 0;
+	lmx->oldpx = lmx->px;
+	lmx->oldpy = lmx->py;
+	movement(lmx);
+	limits(lmx);
+	draw_background(lmx);
+	rays(lmx, 1920, lmx->pa - (30.0 * DR), 0);
+	m_paint(lmx);
+	mlx_put_image_to_window(lmx->lmx, lmx->win, lmx->img, 0, 0);
+	while (c < 20005000)
+		c++;
+	return (1);
+}
+
+static void	m_paint(t_win*lmx)
 {
 	int			ix;
 	int			iy;
-	t_win		*lmx;
 
-	lmx = (t_win *)lm;
 	if (!lmx->w && !lmx->a && !lmx->s && !lmx->d)
-		return (0);
+		return ;
 	ix = -1;
-	limits(lmx);
-	while (++ix <= 4)
+	blackout(lmx, -1, -1);
+	while (++ix < lmx->matrix_s)
 	{
 		iy = -1;
-		while (++iy <= 4)
-		{
-			paint_m(lmx, iy, ix);
-		}
+		while (++iy < lmx->matrix_s)
+			paint_m(lmx, iy, ix, 0);
 	}
-	mlx_put_image_to_window(lmx->lmx, lmx->win, lmx->img, 0, 0);
 	ix = 0;
-	while (ix != 20050000)
-		ix++;
-	return (1);
 }
 
 static int	key_release(int k_code, t_win*lmx)
 {
-	if (k_code == 65307)
-		close_window(lmx);
 	if (k_code == 119)
 		lmx->w = 0;
 	else if (k_code == 97)
@@ -127,6 +328,8 @@ static int	key_release(int k_code, t_win*lmx)
 
 static int	key_press(int k_code, t_win*lmx)
 {
+	if (k_code == 65307)
+		close_window(lmx);
 	if (k_code == 119)
 		lmx->w = 1;
 	else if (k_code == 97)
@@ -138,63 +341,72 @@ static int	key_press(int k_code, t_win*lmx)
 	return (1);
 }
 
-static void	paint_i(t_win*lmx, int iy, int ix)
+static void	paint_m(t_win*lmx, float iy, float ix, char*i)
 {
-	int			x;
-	int			y;
-	char		*i;
+	float		x;
+	float		y;
+	float		x_m;
+	float		y_m;
 
 	x = 64 * ix - 1;
-	while (++x <= 64 * (ix + 1))
+	while (++x < 64 * (ix + 1))
 	{
 		y = 64 * iy - 1;
-		while (++y <= 64 * (iy + 1))
+		while (++y < 64 * (iy + 1))
 		{
-			i = lmx->addr + (y * lmx->ll + x * (lmx->bpp / 8));
-			if (lmx->matrix[iy][ix] == 2 && x >= lmx->px - 2
-				&& x <= lmx->px + 2 && y >= lmx->py - 2 && y <= lmx->py + 2)
-				*(unsigned *)i = 0x008000;
-			if (lmx->matrix[iy][ix] == 1 && x >= 1 && y >= 1 &&
-				x <= 64 *(ix + 1) - 2 && y <= 64 *(iy + 1) - 2)
+			x_m = ((x - lmx->px) + 160);
+			y_m = ((y - lmx->py) + 160);
+			if (x_m < 0 || x_m > 320 || y_m < 0 || y_m > 320)
+				continue ;
+			i = lmx->addr + ((int)y_m * lmx->ll + (int)x_m * (lmx->bpp / 8));
+			if (lmx->matrix[(int)iy][(int)ix] == 1)
 				*(unsigned *)i = 0xFF0000;
+			else if (lmx->matrix[(int)iy][(int)ix] == 0
+				|| lmx->matrix[(int)iy][(int)ix] == 2)
+				*(unsigned *)i = 0x333333;
+			if (x_m >= 158 && x_m <= 162 && y_m >= 158 && y_m <= 162)
+				*(unsigned *)i = 0x00FF00;
 		}
 	}
 }
 
 void	game(t_win*lmx)
 {
+	mlx_put_image_to_window(lmx->lmx, lmx->win, lmx->img, 0, 0);
 	mlx_hook(lmx->win, 17, 0, close_window, lmx);
 	mlx_hook(lmx->win, 2, 1L << 0, key_press, lmx);
 	mlx_hook(lmx->win, 3, 1L << 1, key_release, lmx);
-	mlx_loop_hook(lmx->lmx, m_paint, lmx);
+	mlx_loop_hook(lmx->lmx, paint, lmx);
 	mlx_loop(lmx->lmx);
 }
 
-void	init(t_win*lmx)
+void	init(t_win*lmx, float ix, float iy)
 {
-	int			ix;
-	int			iy;
-
 	lmx->lmx = mlx_init();
-	lmx->px = 0;
-	lmx->py = 0;
-	lmx->win = mlx_new_window(lmx->lmx, 1920, 1920, "teste");
-	lmx->img = mlx_new_image(lmx->lmx, 1920, 1920);
+	lmx->win = mlx_new_window(lmx->lmx, 1920, 1024, "teste");
+	lmx->img = mlx_new_image(lmx->lmx, 1920, 1024);
 	lmx->addr = mlx_get_data_addr(lmx->img, &lmx->bpp, &lmx->ll, &lmx->endian);
-	ix = -1;
+	lmx->pa = (3 * PI) / 2;
+	lmx->pdx = cos(lmx->pa) * 5;
+	lmx->pdy = sin(lmx->pa) * 5;
 	while (++ix <= 4)
 	{
 		iy = -1;
 		while (++iy <= 4)
 		{
-			if (lmx->matrix[iy][ix] == 2 && !lmx->px)
+			if (lmx->matrix[(int)iy][(int)ix] == 2)
 				lmx->px = (64 * (ix + 1)) - 32;
-			if (lmx->matrix[iy][ix] == 2 && !lmx->py)
+			if (lmx->matrix[(int)iy][(int)ix] == 2)
 				lmx->py = (64 * (iy + 1)) - 32;
-			paint_i(lmx, ix, iy);
 		}
 	}
-	mlx_put_image_to_window(lmx->lmx, lmx->win, lmx->img, 0, 0);
+	ix = -1;
+	while (++ix <= 4)
+	{
+		iy = -1;
+		while (++iy <= 4)
+			paint_m(lmx, ix, iy, 0);
+	}
 	game(lmx);
 }
 
@@ -206,6 +418,7 @@ int	main(int argc, char**argv)
 	lmx = malloc(sizeof(t_win));
 	lmx->matrix = ft_calloc(5, 8);
 	argc = -1;
+	lmx->matrix_s = 5;
 	while (++argc <= 4 && argv)
 	{
 		lmx->matrix[argc] = ft_calloc(5, sizeof(float));
@@ -222,6 +435,6 @@ int	main(int argc, char**argv)
 				lmx->matrix[argc][c] = 0;
 		}
 	}
-	init(lmx);
+	init(lmx, -1, -1);
 	return (0);
 }
